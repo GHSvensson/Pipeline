343f41ec095db7487e8179a7318327dc8b5c9488
GHSvensson <122975633+GHSvensson@users.noreply.github.com>
2023-03-19 15:39:54 +0100

    Removed unused imports

--- Tests.py
+++ Tests.py
@@ -1,15 +1,6 @@
-import json
-import time
-
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
 options = Options()
 
