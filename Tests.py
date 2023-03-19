9ac2fa312df0e38700ecd666ba72817cf082dc90
GHSvensson <122975633+GHSvensson@users.noreply.github.com>
2023-03-19 16:36:14 +0100

    Added game functionality test

--- Tests.py
+++ Tests.py
@@ -20,3 +20,18 @@ class TestFullscreentest:
         self.driver.get("https://ghsvensson.github.io/NeonTyper/")
         self.driver.find_element(By.ID, "full-screen-button").click()
         self.driver.find_element(By.ID, "full-screen-button").click()
+        
+class TestGamefunctionalitytest():
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+  
+    def teardown_method(self, method):
+        self.driver.quit()
+  
+    def test_gamefunctionalitytest(self):
+        self.driver.get("https://ghsvensson.github.io/NeonTyper/")
+        self.driver.find_element(By.CSS_SELECTOR, ".active").click()
+        self.driver.find_element(By.ID, "start-button").click()
+        self.driver.execute_script("window.scrollTo(0,75.55555725097656)")
+        self.driver.find_element(By.ID, "return-button").click()
