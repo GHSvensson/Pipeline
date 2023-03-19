2c2f85fb183b3ff36ae98c5fb5bb3afe638ad673
GHSvensson <122975633+GHSvensson@users.noreply.github.com>
2023-03-19 15:07:43 +0100

    Updated according to the superlinter

--- Tests.py
+++ Tests.py
@@ -3,13 +3,13 @@ import time
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument('--headless')
@@ -28,4 +28,3 @@ class TestFullscreentest:
         self.driver.get("https://ghsvensson.github.io/NeonTyper/")
         self.driver.find_element(By.ID, "full-screen-button").click()
         self.driver.find_element(By.ID, "full-screen-button").click()
-  
