3c52944c8d95ad69757abeca735406152d40c98a
GHSvensson <122975633+GHSvensson@users.noreply.github.com>
2023-03-19 14:41:58 +0100

    Updated test to pass linter

--- Tests.py
+++ Tests.py
@@ -9,16 +9,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestFullscreentest():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_fullscreentest(self):
-    self.driver.get("https://ghsvensson.github.io/NeonTyper/")
-    self.driver.find_element(By.ID, "full-screen-button").click()
-    self.driver.find_element(By.ID, "full-screen-button").click()
+class TestFullscreentest:
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_fullscreentest(self):
+        self.driver.get("https://ghsvensson.github.io/NeonTyper/")
+        self.driver.find_element(By.ID, "full-screen-button").click()
+        self.driver.find_element(By.ID, "full-screen-button").click()
   
